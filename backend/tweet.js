9218d34ec67b08f32f7bd4fe592f163e78cdbd83
anthonyanader <anthonyanader@gmail.com>
2018-09-24 22:29:08 -0400

    slight changes

--- backend/tweet.js
+++ backend/tweet.js
@@ -1,5 +1,5 @@
 class Tweet {
-    constructor({ id, content, date, sentiment }){
+    constructor({ id, content, date, sentiment } = {}){
         this.id = id;
         this.content = content;
         this.date = date;
