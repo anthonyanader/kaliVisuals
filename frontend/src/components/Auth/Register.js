066479efc199e0e5f5260dc9b1dbbb06580b0335
anthonyanader <anthonyanader@gmail.com>
2018-10-15 20:42:20 -0400

    linked registration form to to firebase -- user can now login with an email and password and db is automatically populated with user information -- moving onto form validation next

--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import firebase from '../../firebase';
 import { Link } from 'react-router-dom';
 import { Grid, Form, Segment, Button, Header, Message, Icon }from 'semantic-ui-react';
 
 class Register extends React.Component{
-    state = {}
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirmation: ''
+    }
 
-    handleChange = () => {}
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+            console.log(createdUser)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }
 
     render(){
+        const {
+            username,
+            email,
+            password,
+            passwordConfirmation 
+        } = this.state;
+
         return(
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -15,15 +44,16 @@ class Register extends React.Component{
                         <Icon name="chart pie" color="blue" />
                         Register for Kali-Visuals
                     </Header>
-                    <Form size="large">
+                    <Form onSubmit={this.handleSubmit} size="large">
                         <Segment raised>
-                            <Form.Input 
+                        <Form.Input 
                             fluid name="username" 
                             iconPosition="left" 
                             icon="user"
                             placeholder="Username" 
                             onChange={this.handleChange} 
-                            type="text" />
+                            type="text"
+                            value={username} />
 
                             <Form.Input 
                             fluid name="email" 
@@ -31,7 +61,8 @@ class Register extends React.Component{
                             icon="mail"
                             placeholder="Email" 
                             onChange={this.handleChange} 
-                            type="email" />
+                            type="email"
+                            value={email} />
 
                             <Form.Input 
                             fluid name="password" 
@@ -39,7 +70,8 @@ class Register extends React.Component{
                             icon="lock"
                             placeholder="Password" 
                             onChange={this.handleChange} 
-                            type="password" />
+                            type="password"
+                            value={password} />
 
                             <Form.Input
                             fluid name="passwordConfirmation" 
@@ -47,7 +79,8 @@ class Register extends React.Component{
                             icon="repeat"
                             placeholder="Password Confirmation" 
                             onChange={this.handleChange} 
-                            type="password" />
+                            type="password"
+                            value={passwordConfirmation} />
 
                             <Button 
                             color="blue" 
